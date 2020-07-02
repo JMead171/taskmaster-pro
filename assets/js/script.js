6888da09bc9e3c60ff17310493f07cc986d9516c
Jeff Mead <jeffdmead171@gmail.com>
2020-07-02 16:38:07 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,7 +174,7 @@ $("#modalDueDate").datepicker( {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -201,20 +201,22 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(this).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(this).removeClass("dropover-active");
   },
 
   update: function (event) {
-  var tempArr = [];
+    var tempArr = [];
     $(this).children().each(function() {
       var text = $(this)
         .find("p")
@@ -243,6 +245,7 @@ $(".card .list-group").sortable({
 });
 
 var auditTask = function(taskE1) {
+  console.log(taskE1);
   // get date from task element
   var date = $(taskE1).find("span").text().trim();
 
@@ -264,13 +267,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -285,3 +288,9 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) *30);
